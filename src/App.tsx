18805e7d1179845af355d6fb28d42a03e5cad63e
Larry Gust <larry.v.gust@gmail.com>
2025-12-19 10:36:25 -0700

    favicon, title, remove dupe providers

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import { Navbar } from "./components/Navbar";
 import { Account } from "./pages/Account";
 import { MyAccount } from "./pages/account/MyAccount";
 import { OrderHistory } from "./pages/account/OrderHistory";
-import { AppProviders } from "./components/AppProviders";
 import { CategoryList } from "./pages/CategoryList";
 import { ProductsByCategory } from "./pages/ProductsByCategory";
 import { ProductDetails } from "./pages/ProductDetails";
@@ -24,52 +23,50 @@ function App() {
   const { isAuthenticated } = useAuth();
 
   return (
-    <AppProviders>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<CategoryList />} />
-          <Route path="/shop/category/:categoryId" element={<ProductsByCategory />} />
-          <Route path="/shop/product/:productId" element={<ProductDetails />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<CategoryList />} />
+        <Route path="/shop/category/:categoryId" element={<ProductsByCategory />} />
+        <Route path="/shop/product/:productId" element={<ProductDetails />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
 
-          {/* Account Routes with Nested Routing */}
-          <Route path="/account" element={<Account />}>
-            <Route index element={<Navigate to="/account/my-account" replace />} />
-            <Route path="my-account" element={<MyAccount />} />
-            <Route path="orders" element={<OrderHistory />} />
-          </Route>
+        {/* Account Routes with Nested Routing */}
+        <Route path="/account" element={<Account />}>
+          <Route index element={<Navigate to="/account/my-account" replace />} />
+          <Route path="my-account" element={<MyAccount />} />
+          <Route path="orders" element={<OrderHistory />} />
+        </Route>
 
-          <Route path="/request-service" element={<ServiceRequest />} />
-          <Route path="/rent-equipment" element={<EquipmentRental />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/order-confirmation" element={<OrderConfirmation />} />
+        <Route path="/request-service" element={<ServiceRequest />} />
+        <Route path="/rent-equipment" element={<EquipmentRental />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/order-confirmation" element={<OrderConfirmation />} />
 
-          {/* Admin Routes */}
-          <Route
-            path="/admin/service-requests"
-            element={
-              <RequireAdmin>
-                <AdminServiceRequests />
-              </RequireAdmin>
-            }
-          />
-          <Route
-            path="/admin/rental-requests"
-            element={
-              <RequireAdmin>
-                <AdminRentalRequests />
-              </RequireAdmin>
-            }
-          />
+        {/* Admin Routes */}
+        <Route
+          path="/admin/service-requests"
+          element={
+            <RequireAdmin>
+              <AdminServiceRequests />
+            </RequireAdmin>
+          }
+        />
+        <Route
+          path="/admin/rental-requests"
+          element={
+            <RequireAdmin>
+              <AdminRentalRequests />
+            </RequireAdmin>
+          }
+        />
 
-          <Route path="*" element={<Navigate to={isAuthenticated ? "/account" : "/login"} replace />} />
-        </Routes>
-      </BrowserRouter>
-    </AppProviders>
+        <Route path="*" element={<Navigate to={isAuthenticated ? "/account" : "/login"} replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
